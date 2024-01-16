e25c3038d7e957209068af52e99b65b81bb24040
Zeke <zekesmith8@gmail.com>
2024-01-16 11:43:27 -0800

    1

--- template/code/main.py
+++ template/code/main.py
@@ -8,7 +8,7 @@ class Game:
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
+		pygame.display.set_caption('Heros Tale')
 		self.clock = pygame.time.Clock()
 
 		self.level = Level()
